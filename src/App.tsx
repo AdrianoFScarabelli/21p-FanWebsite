3eef348e32bde22cf13d957b0e1afccf1eb1161d
Adriano Scarabelli <adrianoscarabelli@gmail.com>
2026-08-06 17:38:08 -0300

    Adicionando as sections background e sobre

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,28 @@ function App() {
 
   return (
     <div>
-      <div className="header">
+      {/* <div className="hero"></div> */}
+      <header>
         <div>
-          <img src="../public/logo.png" alt="foto da logo" className="logo" />
+          <img src="/logo.png" alt="foto da logo" className="logo" />
         </div>
-        <div className="header-options">
+        <nav className="header-options">
           <h2>SOBRE</h2>
+          <h2>ÁLBUNS</h2>
+          <h2>PERSONAGENS</h2>
+          <h2>REGIÕES</h2>
+        </nav>
+      </header>
+      <section id="background" className="background">
+        <img src="/background.png" alt="foto de fundo" />
+      </section>
+      <section id="sobre" className="sobre">
+        <img src="/sobre-image.png" alt="foto da logo" className="sobre-image" />
+        <div>
+          <h2 style={{fontSize: '50px', fontFamily: 'Anton, sans-serif'}}>SOBRE</h2>
+          <p>Twenty One Pilots é uma banda</p>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
