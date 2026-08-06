e4559662059f86e6d53ccfc50a63611485c44695
Adriano Scarabelli <adrianoscarabelli@gmail.com>
2026-08-05 23:41:04 -0300

    Iniciando desenvolvimento do header

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,12 @@ function App() {
   return (
     <div>
       <div className="header">
-        
+        <div>
+          <img src="../public/logo.png" alt="foto da logo" className="logo" />
+        </div>
+        <div className="header-options">
+          <h2>SOBRE</h2>
+        </div>
       </div>
     </div>
   );
