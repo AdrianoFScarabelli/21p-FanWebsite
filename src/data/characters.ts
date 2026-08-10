a3d87eb36eb6cc99fd69811af1ffafdf81138239
Adriano Scarabelli <adrianoscarabelli@gmail.com>
2026-08-10 19:36:24 -0300

    Adicionando descrição de personagens

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -6,7 +6,7 @@ export const characters: Character[] = [
     name: 'Clancy',
     imageUrl: 'clancy.png',
     accentColor: '#C41E1E',
-    description: 'Clancy é o protagonista...',
+    description: 'Clancy é o protagonista da lore construída pela Twenty One Pilots, um homem preso dentro dos limites de Trench (também chamada de Dema), uma cidade cercada por nove torres de pedra vigiadas pelos misteriosos Bishops.\n Sua história é contada de forma fragmentada ao longo dos álbuns Blurryface, Trench, Scaled and Icy e Clancy, através de letras, videoclipes e símbolos escondidos.\n Ao longo da narrativa, Clancy tenta escapar das muralhas amarelas que isolam Trench do mundo exterior, enfrentando os Bishops — figuras que mantêm os habitantes presos através do medo e da vigilância constante.\n Sua jornada é acompanhada por um grupo de rebeldes chamado Banditos, que também lutam contra o controle imposto pela cidade.\n O álbum Clancy (2024), que encerra essa fase da lore, marca o momento em que ele finalmente confronta Nico, o líder dos Bishops, buscando não só a própria liberdade, mas também resgatar quem ficou para trás em Trench.',
     faction: 'Trench',
   },
   {
